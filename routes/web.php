644ba1a9bae09958a52c54db037d869b3ac04fd3
RJUSM <rustam190401@mail.ru>
2022-05-03 14:02:12 +0300

    CREATE : user & roles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\{RoleController,UserController};
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,8 +26,9 @@ require __DIR__.'/auth.php';
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
 
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    //Route::resource('products', ProductController::class);
 });
