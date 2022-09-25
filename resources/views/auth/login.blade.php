95fe8cb24708fc01a6450c0004f133da21337046
RJUSM <rustam190401@mail.ru>
2022-09-25 21:12:24 +0300

    UPDATE : Guest layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,66 @@
 <x-guest-layout>
-    <a href="/" class="flex justify-center items-center">
-        <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
-    </a>
+    <div class="flex justify-center items-center h-screen bg-gray-200 px-6">
+        <div class="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
+            <a href="/" class="flex justify-center items-center">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
+            </a>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')"/>
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')"/>
 
-    <!-- Validation Errors -->
-    <x-auth-validation-errors class="mb-4 mt-4" :errors="$errors"/>
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4 mt-4" :errors="$errors"/>
 
-    <form method="POST" action="{{ route('login') }}" class="mt-4">
-    @csrf
+            <form method="POST" action="{{ route('login') }}" class="mt-4">
+                @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-label for="email" :value="__('Email')"/>
-            <x-input type="email"
-                     name="email"
-                     id="email"
-                     value="{{ old('email') }}"
-                     required
-                     autofocus
-            />
+                <!-- Email Address -->
+                <div>
+                    <x-label for="email" :value="__('Email')"/>
+                    <x-input type="email"
+                             name="email"
+                             id="email"
+                             value="{{ old('email') }}"
+                             required
+                             autofocus
+                    />
+                </div>
+
+                <!-- Password -->
+                <div class="mt-3">
+                    <x-label for="password" :value="__('Password')"/>
+                    <x-input type="password"
+                             name="password"
+                             id="password"
+                             required
+                             autocomplete="current-password"
+                    />
+                </div>
+
+                <div class="flex justify-between mt-4">
+                    <!-- Remember Me -->
+                    <div>
+                        <label class="inline-flex items-center">
+                            <input type="checkbox" class="form-checkbox text-indigo-600" name="remember">
+                            <span class="mx-2 text-gray-600 text-sm">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+
+                    <div>
+                        @if (Route::has('password.request'))
+                            <a class="block text-sm fontme text-indigo-700 hover:underline"
+                               href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="mt-6">
+                    <x-button class="w-full">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
+
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-3">
-            <x-label for="password" :value="__('Password')"/>
-            <x-input type="password"
-                     name="password"
-                     id="password"
-                     required
-                     autocomplete="current-password"
-            />
-        </div>
-
-        <div class="flex justify-between mt-4">
-            <!-- Remember Me -->
-            <div>
-                <label class="inline-flex items-center">
-                    <input type="checkbox" class="form-checkbox text-indigo-600" name="remember">
-                    <span class="mx-2 text-gray-600 text-sm">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div>
-                @if (Route::has('password.request'))
-                    <a class="block text-sm fontme text-indigo-700 hover:underline"
-                       href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
-                @endif
-            </div>
-        </div>
-
-        <div class="mt-6">
-            <x-button class="w-full">
-                {{ __('Log in') }}
-            </x-button>
-        </div>
-
-    </form>
+    </div>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Admin\{UserController,RoleController};
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
